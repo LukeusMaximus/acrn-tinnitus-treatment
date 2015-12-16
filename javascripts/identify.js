6ec48453bb155ec932b7f49a64da22b5006bae06
LukeusMaximus <lukeusmaximus39@gmail.com>
2015-12-16 20:44:29 +0000

    Created tinnitus frequency identification tool.

--- javascripts/identify.js
+++ javascripts/identify.js
@@ -3,8 +3,14 @@
  */
 
 var audioContext;
-var toneGenerator;
-var playing;
+var toneGenerator = null;
+
+var playing = false;
+var desiredFrequency = 440;
+
+var toneGenToggle;
+var toneFrequencySlider;
+var selectedFrequencySpan;
 
 function initAudioContext() {
 	try {
@@ -18,33 +24,52 @@ function initAudioContext() {
 
 function initToneGenerator() {
 	toneGenerator = audioContext.createOscillator();
-	toneGenerator.frequency.value = 200;
+	toneGenerator.frequency.value = desiredFrequency;
 	toneGenerator.connect(audioContext.destination);
 }
 
+function findElements() {
+	toneGenToggle = document.querySelector("#toneGenToggle");
+	toneFrequencySlider = document.querySelector("#toneFrequencySlider");
+	selectedFrequencySpan = document.querySelector("#selectedFrequencySpan");
+}
+
+function init() {
+	initAudioContext();
+	findElements();
+}
+
 function startTone() {
-	if ( typeof toneGenerator === "undefined") {
-		initToneGenerator();
-	}
-	toneGenerator.start();
+	initToneGenerator();
+	toneGenerator.start(0);
 	playing = true;
+	toneGenToggle.innerText = "Stop tone generator";
 }
 
 function stopTone() {
-	toneGenerator.stop();
-	playing = false;
+	toneGenerator.stop(0);
+	toneGenerator = null;
+	toneGenToggle.innerText = "Start tone generator";
+}
+
+function updateFrequency() {
+	if(typeof selectedFrequencySpan === "undefined") {
+		findElements();
+	}
+	desiredFrequency = toneFrequencySlider.value;
+	selectedFrequencySpan.innerText = desiredFrequency;
+	if ( toneGenerator !== null) {
+		toneGenerator.frequency.value = desiredFrequency;
+	}
 }
 
 function toggleToneGeneration() {
-	if (typeof audioContext === "undefined") {
-		initAudioContext();
+	if ( typeof audioContext === "undefined") {
+		init();
 	}
-	if ( typeof toneGenerator === "undefined") {
-		initToneGenerator();
-	}
-	if (playing) {
-		stopTone();
-	} else {
+	if (toneGenerator === null) {
 		startTone();
+	} else {
+		stopTone();
 	}
 }
